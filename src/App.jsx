f32bb1121297dd67920f4c145cdb12790c23af91
petern160 <peternguyen160@gmail.com>
2019-07-12 20:42:12 -0700

    seperated clock component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
+import Clock from "./Clock";
 
 class App extends Component {
   constructor(props) {
@@ -19,12 +20,7 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-title">Countdown to {this.state.deadline}</div>
-        <div>
-          <div className="Clock-days">14 days</div>
-          <div className="Clock-hours">30 hours</div>
-          <div className="Clock-minutes">15 minutes</div>
-          <div className="Clock-seconds">20 seconds</div>
-        </div>
+        <Clock />
         <div>
           <input
             onChange={event =>
