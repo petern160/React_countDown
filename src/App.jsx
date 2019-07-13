639bd76d4a6f74e6f38f6e79214a78e028cf43c7
petern160 <peternguyen160@gmail.com>
2019-07-12 21:05:25 -0700

    added in time update calculations

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-title">Countdown to {this.state.deadline}</div>
-        <Clock />
+        <Clock deadline={this.state.deadline} />
         <div>
           <input
             onChange={event =>
--- src/Clock.jsx
+++ src/Clock.jsx
@@ -10,6 +10,32 @@ class Clock extends Component {
       minutes: 0,
       seconds: 0
     };
+    console.log("this.props", this.props);
+  }
+
+  getTimeUntil(deadline) {
+    const time = Date.parse(deadline) - Date.parse(new Date());
+    const seconds = Math.floor((time / 1000) % 60);
+    const minutes = Math.floor((time / 1000 / 60) % 60);
+    const hours = Math.floor((time / (1000 * 60 * 60)) % 24);
+    const days = Math.floor(time / (1000 * 60 * 60 * 24));
+
+    // console.log(
+    //   "seconds",
+    //   seconds,
+    //   "minutes",
+    //   minutes,
+    //   "hours",
+    //   hours,
+    //   "days",
+    //   days
+    // );
+
+    this.setState({ days: days, hours, minutes, seconds });
+  }
+
+  componentWillMount() {
+    this.getTimeUntil(this.props.deadline);
   }
 
   render() {
