88e7f5d9a054220346022aa8c8b98b75e6b6028c
petern160 <peternguyen160@gmail.com>
2019-07-12 19:52:09 -0700

    mock layout of countdown

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <div> Countdown to December 25, 2019</div>
+        <div>
+          <div>14 days</div>
+          <div>30 hours</div>
+          <div>15 minutes</div>
+          <div>20 seconds</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
