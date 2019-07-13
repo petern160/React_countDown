8055ff29c6c38e8497909bc10c699ec1cf51a15b
petern160 <peternguyen160@gmail.com>
2019-07-12 20:01:08 -0700

    styled fonts

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div> Countdown to December 25, 2019</div>
+        <div className="App-title"> Countdown to December 25, 2019</div>
         <div>
-          <div>14 days</div>
-          <div>30 hours</div>
-          <div>15 minutes</div>
-          <div>20 seconds</div>
+          <div className="Clock-days">14 days</div>
+          <div className="Clock-hours">30 hours</div>
+          <div className="Clock-minutes">15 minutes</div>
+          <div className="Clock-seconds">20 seconds</div>
         </div>
         <div>
           <input placeholder="new date" />
