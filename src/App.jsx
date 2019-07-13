c276240adb5014844f257d86c0fc53a979de8439
petern160 <peternguyen160@gmail.com>
2019-07-12 19:56:55 -0700

    added in css file and did some basic styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   state = {};
   render() {
     return (
-      <div>
+      <div className="App">
         <div> Countdown to December 25, 2019</div>
         <div>
           <div>14 days</div>
           <div>30 hours</div>
           <div>15 minutes</div>
           <div>20 seconds</div>
+        </div>
+        <div>
+          <input placeholder="new date" />
+          <button>Submit</button>
         </div>
       </div>
     );
